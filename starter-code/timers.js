5124090af4a563bea4340095c3bb2f52b6684f0f
Esther Low <phi21416@gmail.com>
2016-12-15 16:31:59 +0800

    completed

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,32 @@
 // your code here:
+
+var seconds = 0;
+var timerId;
+
+
+$("#start").click(function(){
+timerId = setInterval(updateTime, 1000)
+return seconds;
+})
+
+$("#pause").click(function(){
+clearInterval(timerId);
+console.log("pause fired");
+})
+
+$("#reset").click(function(){
+  clearInterval(timerId);
+  seconds = 0;
+  $('#timer').html("Stop Watch")
+})
+
+function updateTime () {
+seconds = seconds + 1;
+$('#timer').html('Time Elapsed: ' + seconds);
+console.log(seconds);
+}
+
+// 8. Inside your click handler for the reset button...
+//   - Stop the timer using `clearInterval()`.
+//   - Reset the timer.
+//   - Replace the time in your HTML with the original "Stop Watch" text.
